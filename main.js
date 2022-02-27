7311be60c850fe0d58b98d5938ecb8cf43998ea2
awright18 <awright18@gmail.com>
2022-02-27 11:17:19 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+function getEmbedableUrl(){
+    let message = document.querySelector('.message');
+    
+    message.innerText = "";
+
+    let embeddableUrl = document.querySelector('.embeddableUrl');
+
+    let driveUrl = document.querySelector('#driveUrl').value;
+    
+    console.log(driveUrl);
+    
+    if(driveUrl == undefined || !driveUrl.startsWith('https://drive.google.com/file/d/')){
+      alert('bad url');
+      
+      return; 
+    }
+    
+    driveUrl = driveUrl.replace('https://drive.google.com/file/d/',
+                     'https://drive.google.com/uc?export=view&id=' );
+    
+    console.log(driveUrl);
+    
+    driveUrl = driveUrl.replace('/view?usp=sharing','');
+    
+    console.log(driveUrl);
+    
+    let googleDriveImage = document.querySelector('#googleDriveImage');
+    
+    // driveUrl.select();
+    
+    navigator.clipboard.writeText(driveUrl);
+    
+    
+    console.log(googleDriveImage);
+    
+    googleDriveImage.src = driveUrl;
+
+    embeddableUrl.innerText = driveUrl;
+    
+    message.innerText ="Link Copied To Clipboard";
+    
+  }
+
